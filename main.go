6409b5c4c4c3c7aba3e84a3fd17f548522cc6161
Muhammad Ziad Rahmatullah <ziad.muhammad.r@gmail.com>
2024-02-17 19:21:19 +0700

    fitur1

--- main.go
+++ main.go
@@ -6,4 +6,9 @@ func main(){
 	fmt.Print("Computer 44")
 	fmt.Print("Computer 22")
 	fmt.Print("Computer 33")
+	fmt.Print("Computer 33")
+	fmt.Print("Computer 33")
+	fmt.Print("Computer 33")
+	fmt.Print("Computer 33")
+	fmt.Print("Computer 33")
 }
